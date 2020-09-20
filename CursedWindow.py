8a8191de98b150225efba9d78c183fce24301101
nckbrzg <nck@brzbrg.com>
2020-09-20 08:41:47 -0700

    Refactored CursedWindow to accomodate tabs

--- CursedWindow.py
+++ CursedWindow.py
@@ -5,46 +5,49 @@ class CursedWindow():
     def __init__(self, hasBorder, title="Window"):
         begin_x = 20; begin_y = 7
         height = 10; width = 40
+
+        # Initialize active window
         self.hasBorder = hasBorder
-        self.curses_win = curses.newwin(height, width, begin_y, begin_x)
-        self.page_footer_win = curses.newwin(1, width - 2, begin_y + height-1, begin_x + 1)
-        self.title = title
-        self.win_title = curses.newwin(1, len(title) + 4, begin_y, begin_x + 1)
+        self.active_tab = curses.newwin(height, width, begin_y, begin_x)
+
+        # Initialize paging info footer
+        self.active_paging_info = curses.newwin(1, width - 2, begin_y + height-1, begin_x + 1)
+
+        # Initialize title
+        self.active_title = title
 
         return
 
     def clear(self):
-        self.curses_win.clear()
-        self.page_footer_win.clear()
-        self.win_title.clear()
+        self.active_tab.clear()
+        self.active_paging_info.clear()
 
     def refresh(self):
         if self.hasBorder:
-            self.curses_win.box()
-        self.curses_win.refresh()
-        self.page_footer_win.refresh()
-        self.win_title.addstr(0, 1, " {0} ".format(self.title))
+            self.active_tab.box()
 
-        self.win_title.refresh()
+        self.active_tab.addstr(0, 1, " {0} ".format(self.active_title))
+        self.active_tab.refresh()
+        self.active_paging_info.refresh()
 
     def turnOnColorScheme(self, colorScheme):
-        self.curses_win.attron(colorScheme)
+        self.active_tab.attron(colorScheme)
 
     def turnOffColorScheme(self, colorScheme):
-        self.curses_win.attroff(colorScheme)
+        self.active_tab.attroff(colorScheme)
 
     def setBackgroundColor(self, color):
-        self.curses_win.bkgd(' ', color)
+        self.active_tab.bkgd(' ', color)
 
     def renderText(self, text, row, col):
-        self.curses_win.addstr(row, col, text)
+        self.active_tab.addstr(row, col, text)
 
     def getMaxYX(self):
-        return self.curses_win.getmaxyx()
+        return self.active_tab.getmaxyx()
 
     def setPageFooterColor(self, color):
-        self.page_footer_win.bkgd(' ', color)
+        self.active_paging_info.bkgd(' ', color)
 
     def setPageInfoText(self, text):
-        y_max, x_max = self.page_footer_win.getmaxyx()
-        self.page_footer_win.addstr(0, (x_max // 2) - len(text) // 2, text)
+        y_max, x_max = self.active_paging_info.getmaxyx()
+        self.active_paging_info.addstr(0, (x_max // 2) - len(text) // 2, text)
